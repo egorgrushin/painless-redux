36270514f4a74315473793fc31d01e0392f5c9ff
GrushinEgor <egorgrushin@gmail.com>
2019-07-10 14:28:32 +0300

    add missing exported modules

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export * from './store';
 export * from './entity';
 export * from './types';
+export * from './workspace';
 export * from './foreign-keys';
 export { undoReducerFactory, batchActionsReducerFactory } from './reducers/systemFactory';
 export { loadingStateByKeysReducer, loadingStateReducer } from './reducers/loadingStateFactory';
-export { updateAtPath, hashString } from './utils';
+export { updateAtPath, hashString, actionSanitizer, hashIt, affectStateFactory } from './utils';
 export * from './action-creators/system';
 export * from './store-connecting.module';
