09749deca524a1d2bb40af60c6adba3d07d3fa30
Egor Grushin <egorgrushin@gmail.com>
2020-12-21 19:57:19 +0300

    now all remote actions can ignore global loading state

--- src/entity/reducers/loading-state.ts
+++ src/entity/reducers/loading-state.ts
@@ -2,6 +2,7 @@ import { LoadingState } from '../../system-types';
 import { createLoadingStateReducer } from '../../shared/loading-state/reducers';
 import { EntityActionTypes } from '../types';
 import { EntityActions } from '../actions';
+import { RequestOptions } from '../../shared/types';
 
 export const createEntityLoadingStateReducer = (
     types: EntityActionTypes,
@@ -11,5 +12,6 @@ export const createEntityLoadingStateReducer = (
 ) => {
     const loadingStateReducer = createLoadingStateReducer(types);
     if (action.type !== types.SET_LOADING_STATE) return state;
+    if ((action.options as RequestOptions)?.globalLoadingState === false) return state;
     return loadingStateReducer(state, action);
 };
